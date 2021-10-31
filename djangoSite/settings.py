130a83f6f6754d022d855ece516397cff60243ee
shahbaz <mohdshahbaz@gmail.com>
2021-10-31 16:06:51 +0530

    fixes

--- djangoSite/settings.py
+++ djangoSite/settings.py
@@ -81,8 +81,19 @@ WSGI_APPLICATION = 'djangoSite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'root',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
